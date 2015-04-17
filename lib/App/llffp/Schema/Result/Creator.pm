179e99000832420f11a914fef7d4bf7dcd9dc1a8
Michael McClimon <michael@mcclimon.org>
2015-04-16 22:43:43 -0400

    Some initial classes for DBIx::Class.
    The DB is nominally versioned, but I haven't done anything to make that
    actually happen yet.

---
+++ lib/App/llffp/Schema/Result/Creator.pm
@@ -0,0 +1,39 @@
+package App::llffp::Schema::Result::Creator;
+use base qw{DBIx::Class::Core};
+
+use warnings;
+use strict;
+
+# This class uses the "creator" table
+__PACKAGE__->table('creator');
+
+__PACKAGE__->add_columns(
+    id => {
+        accessor => 'id',
+        data_type => 'integer',
+        is_nullable => 0,
+        is_auto_increment => 1,
+    },
+    last_name => {
+        data_type => 'varchar',
+        size => 256,
+        is_nullable => 0,
+    },
+    first_name => {
+        data_type => 'varchar',
+        size => 256,
+        is_nullable => 1,
+    },
+    birth_date => {
+        data_type => 'integer',
+        is_nullable => 1,
+    },
+    death_date => {
+        data_type => 'integer',
+        is_nullable => 1,
+    },
+);
+
+__PACKAGE__->set_primary_key('id');
+
+1;
